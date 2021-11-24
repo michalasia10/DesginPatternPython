b8ef85d5bdd14f2407db7c9efd071fbf7f70a037
michalasia10 <misiek1027@o2.pl>
2021-11-24 23:25:13 +0100

    [factories] proper way to make factorymethod

--- Factories/FactoryMethod.py
+++ Factories/FactoryMethod.py
@@ -7,12 +7,31 @@ class CoordinateSystem(Enum):
     POLAR = 2
 
 
+# class Point:
+#     def __init__(self,a,b,system=CoordinateSystem.CARTESIAN):
+#         if system == CoordinateSystem.CARTESIAN:
+#             self.x = a
+#             self.y = b
+#         elif system == CoordinateSystem.POLAR:
+#             self.x = a *cos(b)
+#             self.y = a *sin(b)
+
 class Point:
-    def __init__(self,a,b,system=CoordinateSystem.CARTESIAN):
-        if system == CoordinateSystem.CARTESIAN:
-            self.x = a
-            self.y = b
-        elif system == CoordinateSystem.POLAR:
-            self.x = a *cos(b)
-            self.y = a *sin(b)
-        
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return f'x: {self.x}, y: {self.y}'
+
+    @staticmethod
+    def new_cartehsian_point(x,y):
+        return Point(x,y)
+
+    @staticmethod
+    def new_polar_point(rho,theta):
+        return Point(rho*cos(theta),rho*sin(theta))
+
+if __name__ == '__main__':
+    p = Point(2,3)
+    p2 = Point.new_polar_point(1,2)
+    print('carthesian point',p,'polar point',p2)
