3d4e158e026bbf2593ff7f6dd6273af48bd00484
michalasia10 <misiek1027@o2.pl>
2021-11-24 23:32:42 +0100

    [factories] factory

--- Factories/FactoryMethod.py
+++ Factories/FactoryMethod.py
@@ -17,21 +17,25 @@ class CoordinateSystem(Enum):
 #             self.y = a *sin(b)
 
 class Point:
-    def __init__(self,x,y):
+    def __init__(self, x = 0, y = 0):
         self.x = x
         self.y = y
+
     def __str__(self):
         return f'x: {self.x}, y: {self.y}'
 
+
+class PointFactory:
     @staticmethod
-    def new_cartehsian_point(x,y):
-        return Point(x,y)
+    def new_cartehsian_point(x, y):
+        return Point(x, y)
 
     @staticmethod
-    def new_polar_point(rho,theta):
-        return Point(rho*cos(theta),rho*sin(theta))
+    def new_polar_point(rho, theta):
+        return Point(rho * cos(theta), rho * sin(theta))
+
 
 if __name__ == '__main__':
-    p = Point(2,3)
-    p2 = Point.new_polar_point(1,2)
-    print('carthesian point',p,'polar point',p2)
+    p = PointFactory.new_cartehsian_point(2, 3)
+    p2 = PointFactory.new_polar_point(1, 2)
+    print('carthesian point', p, 'polar point', p2)
