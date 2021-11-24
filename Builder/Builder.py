f2fb43d66b17eed2b66b9e7b14ae916e56bfc2f7
michalasia10 <misiek1027@o2.pl>
2021-11-24 22:42:48 +0100

    [builder] first complete builder

--- Builder/Builder.py
+++ Builder/Builder.py
@@ -1,21 +1,21 @@
 class HtmlElement:
     indent_size = 2
 
-    def __init__(self,name='',text=''):
-        self.name = name
-        self.text = text
-        self.elements = []
+    def __init__(self, name='', text=''):
+        self.name: str = name
+        self.text: str = text
+        self.elements: list = []
 
-    def __str(self,indent):
-        lines = []
-        i = ' '*(indent*self.indent_size)
+    def __str(self, indent):
+        lines: list = []
+        i : str = ' ' * (indent * self.indent_size)
         lines.append(f'{i}<{self.name}')
 
         if self.text:
-            i1 =  ' '*((indent+1) * self.indent_size)
+            i1 = ' ' * ((indent + 1) * self.indent_size)
             lines.append(f'{i1}{self.text}')
         for e in self.elements:
-            lines.append(e.__str(indent+1))
+            lines.append(e.__str(indent + 1))
         lines.append(f'{i}<{self.name}')
         return '\n'.join(lines)
 
@@ -24,6 +24,30 @@ class HtmlElement:
 
 
 class HtmlBuilder:
-    def __init__(self,root_name):
-        self.root_name = root_name
-        self.__root = HtmlElement(name=root_name)
+    def __init__(self, root_name):
+        self.root_name :str = root_name
+        self.__root : HtmlElement = HtmlElement(name=root_name)
+
+    def add_child(self, child_name, child_text):
+        self.__root.elements.append(
+            HtmlElement(child_name, child_text)
+        )
+    def add_child_fluent(self, child_name, child_text):
+        self.__root.elements.append(
+            HtmlElement(child_name, child_text)
+        )
+        return self
+
+    def __str__(self):
+        return str(self.__root)
+
+builder = HtmlBuilder('ul')
+
+#without return self
+builder.add_child('li','hello')
+builder.add_child('li','world')
+
+#with return self
+builder.add_child_fluent('li','fluent').add_child_fluent('li','fluent_second')
+
+print(f"Ordinary builder\n {builder}")
