2b4e5baf7662fceea8fce09b0ec72d77584c91aa
michalasia10 <misiek1027@o2.pl>
2021-11-27 21:27:08 +0100

    [prototypes] standard class for person with adress

---
+++ Prototype/Prototype.py
@@ -0,0 +1,24 @@
+
+class Address:
+    def __init__(self,street_addres,city,country):
+        self.street_adress = street_addres
+        self.city = city
+        self.country = country
+
+    def __str__(self):
+        return f'{self.street_adress}, {self.city}, {self.country}'
+
+
+
+
+class Person:
+    def __init__(self,name,address):
+        self.name = name
+        self.address = address
+
+    def __str__(self):
+        return f'{self.name} lives at {self.address}'
+
+
+john = Person('John',Address('123 London Road','London','UK'))
+print(john)
